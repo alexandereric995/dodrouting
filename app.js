ed67fab8e283c879ba8191eae1e22d8f9bf42767
Alexander Eric <70110009+alexandereric995@users.noreply.github.com>
2021-03-19 04:12:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 angular.module('hashbang-mode', ['fake-browser', 'address-bar'])
 
 // Configure the fakeBrowser. Do not set these values in actual projects.
-.constant('initUrl', 'http://www.example.com/base/index.html#!/path?a=b#h')
-.constant('baseHref', '/base/index.html')
+.constant('initUrl', 'https://rmp.spab.gov.my/eApps')
+.constant('baseHref', '/system/index.html')
 .value('$sniffer', { history: false })
 
 .config(function($locationProvider) {
